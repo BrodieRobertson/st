2df185292a753ec4d7f5e93db8dea034fe953af7
Brodie Robertson <brodierobertson54321@gmail.com>
2019-09-05 14:27:03 +0930

    Apply theme and modify some hotkeys

--- config.def.h
+++ config.def.h
@@ -177,22 +177,23 @@ MouseKey mkeys[] = {
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
-	/* mask                 keysym          function        argument */
-	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
-	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	/* mask                 keysym              function        argument */
+	{ XK_ANY_MOD,           XK_Break,           sendbreak,      {.i =  0} },
+	{ ControlMask,          XK_Print,           toggleprinter,  {.i =  0} },
+	{ ShiftMask,            XK_Print,           printscreen,    {.i =  0} },
+	{ XK_ANY_MOD,           XK_Print,           printsel,       {.i =  0} },
+	{ TERMMOD,              XK_plus,            zoom,           {.f = +1} },
+	{ TERMMOD,              XK_underscore,      zoom,           {.f = -1} },
+	{ TERMMOD,              XK_parenright,      zoomreset,      {.f =  0} },
+	{ TERMMOD,              XK_C,               clipcopy,       {.i =  0} },
+	{ TERMMOD,              XK_V,               clippaste,      {.i =  0} },
+	{ TERMMOD,              XK_Y,               selpaste,       {.i =  0} },
+	{ ShiftMask,            XK_Insert,          selpaste,       {.i =  0} },
+	{ TERMMOD,              XK_Num_Lock,        numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,         kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,       kscrolldown,    {.i = -1} },
 };
+
 
 /*
  * Special keys (change & recompile st.info accordingly)
